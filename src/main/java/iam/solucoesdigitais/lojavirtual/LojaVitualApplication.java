893fcbe4e33e60dafeca02670d7d2717340cd69f
Isaac Martiniano <isaacam517@gmail.com>
2024-03-09 16:52:02 -0300

    commit inicial projeto loja virtual com java

---
+++ src/main/java/iam/solucoesdigitais/lojavirtual/LojaVitualApplication.java
@@ -0,0 +1,13 @@
+package iam.solucoesdigitais.lojavirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaVitualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaVitualApplication.class, args);
+	}
+
+}
---
+++ src/test/java/iam/solucoesdigitais/lojavirtual/LojaVitualApplicationTests.java
@@ -0,0 +1,13 @@
+package iam.solucoesdigitais.lojavirtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaVitualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
