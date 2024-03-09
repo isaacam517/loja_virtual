5f2bbe6e795471ea9f16a43ed817b0577a300173
Isaac Martiniano <isaacam517@gmail.com>
2024-03-09 18:59:56 -0300

    Primeira conexao com o banco, usando o MySQL

---
+++ src/main/java/iam/solucoesdigitais/lojavirtual/model/MarcaProduto.java
@@ -0,0 +1,39 @@
+package iam.solucoesdigitais.lojavirtual.model;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "marca_produto")
+public class MarcaProduto implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String nomeDesc;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNomeDesc() {
+		return nomeDesc;
+	}
+
+	public void setNomeDesc(String nomeDesc) {
+		this.nomeDesc = nomeDesc;
+	}
+
+}
