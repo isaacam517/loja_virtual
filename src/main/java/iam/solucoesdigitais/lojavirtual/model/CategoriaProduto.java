48b5eae7045f291e3004e039dbb0e8f4a09362fd
Isaac Martiniano <isaacam517@gmail.com>
2024-03-10 07:23:23 -0300

    Java 17 > 11, Spring Boot 3.2.3 > 2.5.6, db MySQL > PostgresSQL

--- src/main/java/iam/solucoesdigitais/lojavirtual/model/CategoriaProduto.java
+++ src/main/java/iam/solucoesdigitais/lojavirtual/model/CategoriaProduto.java
@@ -1,6 +1,7 @@
 package iam.solucoesdigitais.lojavirtual.model;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -11,16 +12,16 @@ import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "marca_produto")
-@SequenceGenerator(name = "seq_marca_produto", sequenceName = "seq_marca_produto", allocationSize = 1, initialValue = 1)
-public class MarcaProduto implements Serializable {
+@Table(name = "categoria_produto")
+@SequenceGenerator(name = "seq_categoria_produto", sequenceName = "seq_categoria_produto", allocationSize = 1, initialValue = 1)
+public class CategoriaProduto implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_marca_produto")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_categoria_produto")
 	private Long id;
-
+	
 	@Column(name = "nome_descricao", nullable = false)
 	private String nomeDesc;
 
@@ -40,4 +41,27 @@ public class MarcaProduto implements Serializable {
 		this.nomeDesc = nomeDesc;
 	}
 
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CategoriaProduto other = (CategoriaProduto) obj;
+		return Objects.equals(id, other.id);
+	}
+
+
+	
+	
+	
+	
+	
 }
