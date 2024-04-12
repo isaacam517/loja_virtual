043d41d5bc221dc7683f7b40f7a6569aea819023
isaacam517 <isaacam517@gmail.com>
2024-04-12 12:08:47 -0300

    Etapa JPA finalizada

--- src/main/java/iam/solucoesdigitais/lojavirtual/LojaVitualApplication.java
+++ src/main/java/iam/solucoesdigitais/lojavirtual/LojaVitualApplication.java
@@ -2,8 +2,16 @@ package iam.solucoesdigitais.lojavirtual;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EntityScan(basePackages = "iam.solucoesdigitais.lojavirtual.service")
+@ComponentScan(basePackages = {"iam.*"})
+@EnableJpaRepositories(basePackages = {"iam.solucoesdigitais.lojavirtual.repository"})
+@EnableTransactionManagement
 public class LojaVitualApplication {
 
 	public static void main(String[] args) {
